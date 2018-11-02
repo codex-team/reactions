16a86c842f73d7ce5d978050977dd0c041f1e708
Baev Ivan <ivan.baev.14@gmail.com>
2018-11-02 21:57:15 +0300

    Code style fix

--- src/connector/connector.ts
+++ src/connector/connector.ts
@@ -1,13 +1,13 @@
-import * as io from 'socket.io-client';
-import ReactionInfo from './../reaction-info/reaction-info.ts';
+import * as io from 'socket.io-client'
+import ReactionInfo from './../reaction-info/reaction-info.ts'
 
 /**
  * Instrument to create socket connection
  */
 export default class Connector {
 
-  update: (reactions: ReactionInfo[]) => void;
-  socket: any;
+  update: (reactions: ReactionInfo[]) => void
+  socket: any
 
   /**
    * Creates an instance of Connector
@@ -16,24 +16,23 @@ export default class Connector {
    * @param {string} serverIP - IP of the server
    * @param {function} update - function which gets data and updates counters
    */
-  constructor(serverIP: string, update: (reactions: ReactionInfo[]) => void) {
+  constructor (serverIP: string, update: (reactions: ReactionInfo[]) => void) {
 
-    this.socket = io(serverIP);
-    this.update = update;
+    this.socket = io(serverIP)
+    this.update = update
 
-    this.socket.on('message', (msg: any) => update(msg));
+    this.socket.on('message', (msg: any) => update(msg))
 
   }
-  
+
   /**
    * Sends index of the reaction to the server
    *
    * @this {Connector}
    * @this {number} ind - index of the reaction
    */
-  send(ind: number): void {
-    this.socket.send(ind);
+  send (ind: number): void {
+    this.socket.send(ind)
   }
 
 }
-
--- src/reaction-info/reaction-info.ts
+++ src/reaction-info/reaction-info.ts
@@ -3,9 +3,9 @@
  */
 export default class ReactionInfo {
 
-  value: number;
-  userReaction: boolean;
-  
+  value: number
+  userReaction: boolean
+
   /**
    * Creates an instance of ReactionInfo
    *
@@ -13,30 +13,30 @@ export default class ReactionInfo {
    * @param {number}  value - value of the counter
    * @param {boolean} userReaction - reaction of the user
    */
-  constructor(value: number, userReaction: boolean) {
-    this.value = value;
-    this.userReaction = userReaction;
+  constructor (value: number, userReaction: boolean) {
+    this.value = value
+    this.userReaction = userReaction
   }
 
   /**
    * Returns value of the counter
    */
-  getRate(): number {
-    return this.value;
+  getRate (): number {
+    return this.value
   }
 
   /**
    * Returns reactions of the user
    */
-  isVoted(): boolean {
-    return this.userReaction;
+  isVoted (): boolean {
+    return this.userReaction
   }
 
   /**
    * Returns string representation of the ReactionInfo
    */
-  toString(): string {
-    return this.getRate() + ' ' + this.isVoted() + '\n';
+  toString (): string {
+    return this.getRate() + ' ' + this.isVoted() + '\n'
   }
 
 }
