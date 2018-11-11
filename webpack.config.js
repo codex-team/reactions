b7d40f79885ae0505ac814a4d3bad87009ffaa4b
sirpravdi <ollias17@gmail.com>
2018-11-12 00:38:52 +0300

    start migration to postcss

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,10 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          {
+            loader: 'postcss-loader'
+          }
         ]
       },
       {
