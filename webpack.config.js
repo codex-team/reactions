fb755fd425c722f2da0ca2ec51ad8bb2f14a07f5
sirpravdi <ollias17@gmail.com>
2018-11-15 17:32:59 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,12 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader',
+          {
+            loader: 'css-loader', 
+            options: {
+              importLoaders: 1
+            }
+          },
           {
             loader: 'postcss-loader',
             options: {
