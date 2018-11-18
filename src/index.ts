bc459428c076931c2a6ec01af798364e2f5b31b0
sirpravdi <ollias17@gmail.com>
2018-11-18 22:49:35 +0300

    resolve conflict

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 import './index.css';
 import Reactions from './main.ts';
+=======
+import './styles/index.css';
+import Reactions from './ReactionsModule.ts';
+>>>>>>> a4dea86... added styles dir + variables.css + nesting
 export default Reactions;
