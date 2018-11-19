e7e130f71056f3fbf04ca7b37b6c5f4c38358eaf
sirpravdi <ollias17@gmail.com>
2018-11-19 21:10:41 +0300

    delete package-lock.json + fix styles + delete unnecessary files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import './styles/index.css';
-=======
 import './index.css';
->>>>>>> 18ee1f149f831a9aad920f94adf4c4c8fc72a881
 import Reactions from './ReactionsModule.ts';
 export default Reactions;
