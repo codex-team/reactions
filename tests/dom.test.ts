ce00003760b2060c486cf5c278eaf29a3c876e90
timakasucces <timakasucces@users.noreply.github.com>
2018-11-22 16:07:02 +0300

    added tests for DOM

--- tests/dom.test.ts
+++ tests/dom.test.ts
@@ -13,12 +13,29 @@ describe('DOM', () => {
   });
 
   it('should correctly create elements with multiple classes', () => {
-    const testClassList: string[] = ['test1', 'test2', 'test3'];
-    const testElement: HTMLElement = DOM.make('div', testClassList);
+    const testClassList: string[] = ['testClass1', 'testClass2', 'testClass3'];
+    const testTextContent: string = 'testTextContent';
+    const testElement: HTMLElement = DOM.make('div', testClassList, { textContent: testTextContent });
 
     assert.equal(testElement.classList.length, testClassList.length);
+
+    assert.equal(testElement.tagName, 'DIV');
+
     for (const i in testClassList) {
       assert.equal(testElement.classList[i], testClassList[i]);
     }
+
+    assert.equal(testElement.textContent, testTextContent);
+  });
+
+  it('should correctly create elements with single class', () => {
+    const testClass: string = 'testClass';
+    const testElement: HTMLElement = DOM.make('div', testClass);
+
+    assert.equal(testElement.classList.length, 1);
+
+    assert.equal(testElement.tagName, 'DIV');
+
+    assert.equal(testElement.classList[0], testClass);
   });
 });
