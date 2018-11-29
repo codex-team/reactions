5941b42adbd707bce61d5c8013f129d2e79b01ee
Куштанов Руслан Камилевич <kushtush@mail.ru>
2018-11-29 19:40:38 +0300

    update storage.test
    add blank lines

--- tests/storage.test.ts
+++ tests/storage.test.ts
@@ -4,30 +4,38 @@ import { LocalStorageMock } from './mock';
 describe('Storage', () => {
   let Storage;
   (<any> global).localStorage = new LocalStorageMock();
+
   before(() => {
     Storage = require('../src/utils/storage').default;
   });
+
   describe('getItem',() => {
     localStorage.setItem('5','test');
+
     it('should correctly return value from localStorage', () => {
       assert.equal(Storage.getItem('5'), 'test');
       assert.equal(Storage.getItem(5), 'test');
     });
+
     it('should return undefined if key is wrong', () => {
       assert.equal(Storage.getItem('wrong'), undefined);
     });
   });
+
   describe('setItem',() => {
     it('should correctly set value', () => {
       Storage.setItem(5,3);
       assert.equal(localStorage.getItem('5'), '3');
     });
   });
+
   describe('getInt',() => {
     localStorage.setItem('test','5');
+
     it('should correctly return integer from localStorage', () => {
       assert.equal(Storage.getItem('test'), 5);
     });
+
     it('should return undefined if key or value is wrong', () => {
       assert.equal(Storage.getItem('wrong'), undefined);
       localStorage.setItem('test','test5');
